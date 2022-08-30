2707802d5a6ac9173c9c89288d4746c358cdda91
jafuibom <110940408+jafuibom@users.noreply.github.com>
2022-08-30 18:18:06 -0400

    Add files via upload

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by ICMMAC07-13C3 on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
